677670ffbb5dcec45bb6af1b091d06180b58bb48
Elko <129769199+elkogerville@users.noreply.github.com>
2023-05-07 22:03:19 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+     name = "MSGpy",
+     version = "0.1",
+     author = "Elko Gerville-Reache",
+     author_email = "elko.gerville-reache@yale.edu",
+     description = "python program that simulates the interaction of merging galaxies using restricted N-Body calculations",
+     packages = ["MSGpy"]
+)
