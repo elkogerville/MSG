bd1cd36d95a414b93c28f861349b9e2e64978c98
Elko <129769199+elkogerville@users.noreply.github.com>
2023-05-07 22:08:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ setuptools.setup(
      version = "0.1.0",
      author = "Elko Gerville-Reache",
      author_email = "elko.gerville-reache@yale.edu",
-     license='MIT',
+     license = 'MIT',
      description = "python program that simulates the interaction of merging galaxies using restricted N-Body calculations",
-     packages = ["MSGpy"],
-     install_requires=['numpy','matplotlib','random']
+     packages = ["MSGpy"]
 )
